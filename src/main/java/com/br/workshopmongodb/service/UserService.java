0a4e4ddc48fb2fdd79c94b5ed7f3ad5853c1d01e
Breno Ganem <bgp.breno@gmail.com>
2025-04-17 13:07:34 -0300

    User by id and ExceptionHandler

--- src/main/java/com/br/workshopmongodb/service/UserService.java
+++ src/main/java/com/br/workshopmongodb/service/UserService.java
@@ -1,6 +1,7 @@
 package com.br.workshopmongodb.service;
 
 import com.br.workshopmongodb.entity.User;
+import com.br.workshopmongodb.exceptions.ObjectNotFoundException;
 import com.br.workshopmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,4 +17,9 @@ public class UserService {
     return repo.findAll();
 
     }
+
+    public User findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
