6222aaca1f9b82b7a0569ebd0ace101370342e9d
Breno Ganem <bgp.breno@gmail.com>
2025-04-24 17:03:30 -0300

    Criando e acrescentando os comentários aos Posts

--- src/main/java/com/br/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/br/workshopmongodb/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.br.workshopmongodb.config;
 
 import com.br.workshopmongodb.dto.AuthorDTO;
+import com.br.workshopmongodb.dto.CommentDTO;
 import com.br.workshopmongodb.entity.Post;
 import com.br.workshopmongodb.entity.User;
 import com.br.workshopmongodb.repository.PostRepository;
@@ -40,6 +41,12 @@ public class Instantiation implements CommandLineRunner {
         Post post = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/12/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("21/12/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("21/12/2018"), new AuthorDTO(alex));
+
+        post.getComments().addAll(Arrays.asList(c1, c2));
+        post1.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post, post1 ));
 
